bd135542ce7a08e7e5bed188d8582e47c79f41fb
linbc <linbc@aliyun.com>
2016-01-28 20:04:02 +0800

    测试插入和读取

--- src/main.lua
+++ src/main.lua
@@ -26,17 +26,15 @@ function main( )
 	doc:open(U2G('D:/git/excel-helper/t.xlsx'))
 	local sheet = doc:getSheet('Sheet1')
 	assert(sheet)
---	test_write(sheet)
---	doc:save()
+	test_write(sheet)
+	doc:save()
 
 	--测试一下读取数据后打印输出
 	local data = sheet:getRange('A1',10, 1000)
 	for i=1,1000 do
 		local row = data[i]
 		print('row:	',i, row and row[1] or nil,row and row[2] or nil)
-		--row[3] = string.char(string.byte('a')+i)		
 	end
-	
 	
 	doc:close()
 	print('over!')
